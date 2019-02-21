27273208f0be3f26fdf7fb22533d1ff7ce2880d0
Phoique <official.phoique@gmail.com>
2019-02-22 01:26:36 +0300

    Author Schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,33 +1,64 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { 
+  GraphQLObjectType, 
+  GraphQLString, 
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
 
 var Books = [
-  { id: '1', name: 'Dünya Yeni bir başlangıç', genre: 'Bilim kurgu' },
-  { id: '2', name: 'İnsanız ayıbı yok', genre: 'Kişisel gelişim' },
-  { id: '3', name: 'Bir Psikiyatristin Gizli Defteri', genre: 'Piskolojik' },
+  { id: '1', name: 'İçimizdeki Şeytan', genre: 'Edebiyat' },
+  { id: '2', name: 'Benim Kedilerim', genre: 'Deneme' },
+  { id: '3', name: 'İkinci Waliz', genre: 'Şiir' },
+];
 
+var Authors = [
+  { id: '1', name: 'Sabahattin Ali', age: 25 },
+  { id: '2', name: 'Gürbüz Doğan Ekşioğlu', age: 32 },
+  { id: '3', name: 'Küçük İskender', age: 39 }
 ];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
 });
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
 const RootQuery = new GraphQLObjectType({
-  name: 'rootQueryType',
+  name: 'RootQueryType',
   fields: {
+
     book: {
       type: BookType,
-      args: {id: { type: GraphQLString }},
+      args: {id: { type: GraphQLID }},
       resolve(parent, args) {
-        return Books.find(book => book.id === args.id);
+        return _.find(Books, {id: args.id});
+      }
+    },
+
+    author: {
+      type: AuthorType,
+      args: {id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return _.find(Authors, {id: args.id});
       }
     }
+
   }
 });
 
