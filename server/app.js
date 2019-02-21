4376146b9d9dbb651e74d85a76454bce17b1dd65
Phoique <official.phoique@gmail.com>
2019-02-22 00:41:58 +0300

    GraphQL Schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
 
 app.get('/', (request, response) => response.send('Hello'));
 
---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'rootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {id: { type: GraphQLString }},
+      resolve(parent, args){
+
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
