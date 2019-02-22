3e405aed0635819051cf7a1e4f845fcc04acc152
Phoique <official.phoique@gmail.com>
2019-02-23 02:05:39 +0300

    BookList component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import BookList from'./BookList';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>Hello</p>
+        <BookList />
       </div>
     );
   }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ol>
+          <li> Book name </li>
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default BookList;
