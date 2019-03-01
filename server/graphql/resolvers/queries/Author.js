46d19ec3ad14c468f5a2395fa0e2a1ed108a48eb
Phoique <official.phoique@gmail.com>
2019-03-02 00:50:16 +0300

    Author resolvers bug fix

--- server/graphql/resolvers/queries/Author.js
+++ server/graphql/resolvers/queries/Author.js
@@ -8,14 +8,14 @@ const Author = {
       await authorModel.findById(args.id),
     
     authors: async (_, args) => 
-      await authorModel.find({});
+      await authorModel.find({})
   },
 
   Author: {
     books: async (parent) => 
-      await bookModel.find({ parent.author_id })
+      await bookModel.find({ author_id: parent.id })
   },
 
 };
 
-module.exports Author;
+module.exports = Author;
